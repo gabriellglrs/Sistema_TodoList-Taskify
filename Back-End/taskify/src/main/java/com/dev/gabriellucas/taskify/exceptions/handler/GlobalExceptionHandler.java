48e98a1093d94d3cb452590c698381506a473e0c
gabriellglrs <gabriellglrs@gmail.com>
2025-01-24 23:51:42 -0300

    Tratamento de validações de DTO

--- Back-End/taskify/src/main/java/com/dev/gabriellucas/taskify/exceptions/handler/GlobalExceptionHandler.java
+++ Back-End/taskify/src/main/java/com/dev/gabriellucas/taskify/exceptions/handler/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.dev.gabriellucas.taskify.controllers.handler;
+package com.dev.gabriellucas.taskify.exceptions.handler;
 
 import com.dev.gabriellucas.taskify.exceptions.DatabaseException;
 import com.dev.gabriellucas.taskify.exceptions.ResourceNotFoundException;
@@ -6,32 +6,26 @@ import com.dev.gabriellucas.taskify.DTO.ErroResponseDTO;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
      @ExceptionHandler(ResourceNotFoundException.class)
      public ResponseEntity<ErroResponseDTO> resourceNotFound(ResourceNotFoundException exception, HttpServletRequest httpServletRequest) {
 
-          // Extrair o ID do usuário do caminho da URI
-          String path = httpServletRequest.getRequestURI();
-          String userId = path.substring(path.lastIndexOf('/') + 1);
-          String resourceName = path.substring(path.lastIndexOf('/', path.lastIndexOf('/') - 1) + 1, path.lastIndexOf('/'));
-
-          // Remover o 's' final, se existir
-          if (resourceName.endsWith("s")) {
-               resourceName = resourceName.substring(0, resourceName.length() - 1);
-          }
-
                   ErroResponseDTO erroResponseDTO = ErroResponseDTO.builder()
                   .timestamp(Instant.now())
                   .status(HttpStatus.NOT_FOUND.value())
                   .code(HttpStatus.NOT_FOUND.name())
                   .message(exception.getMessage())
-                  .details("Não foi possível localizar um " + resourceName + " com o ID: " + userId + " fornecido. Por favor, verifique o ID e tente novamente. Se o problema persistir, entre em contato com o suporte.")
+                  .details("Não foi possível localizar o ID fornecido. Por favor, verifique o ID e tente novamente. Se o problema persistir, entre em contato com o suporte.")
                   .path(httpServletRequest.getRequestURI())
                   .build();
           return ResponseEntity.status(erroResponseDTO.getStatus()).body(erroResponseDTO);
@@ -40,25 +34,35 @@ public class GlobalExceptionHandler {
      @ExceptionHandler(DatabaseException.class)
      public ResponseEntity<ErroResponseDTO> database(DatabaseException exception, HttpServletRequest httpServletRequest) {
 
-          // Extrair o ID do usuário do caminho da URI
-          String path = httpServletRequest.getRequestURI();
-          String userId = path.substring(path.lastIndexOf('/') + 1);
-          String resourceName = path.substring(path.lastIndexOf('/', path.lastIndexOf('/') - 1) + 1, path.lastIndexOf('/'));
-
-          // Remover o 's' final, se existir
-          if (resourceName.endsWith("s")) {
-               resourceName = resourceName.substring(0, resourceName.length() - 1);
-          }
-
           ErroResponseDTO erroResponseDTO = ErroResponseDTO.builder()
                   .timestamp(Instant.now())
                   .status(HttpStatus.BAD_REQUEST.value())
                   .code(HttpStatus.BAD_REQUEST.name())
                   .message(exception.getMessage())
-                  .details("A entidade " + resourceName + " com ID: " + userId + " não pode ser deletada porque está associada com outra entidade. Para prosseguir com a exclusão, certifique-se de remover ou desassociar todas as entidades relacionadas.")
-                  .path(path)
+                  .details("A entidade não pode ser deletada porque está associada com outra entidade. Para prosseguir com a exclusão, certifique-se de remover ou desassociar todas as entidades relacionadas.")
+                  .path(httpServletRequest.getRequestURI())
                   .build();
           return ResponseEntity.status(erroResponseDTO.getStatus()).body(erroResponseDTO);
+     }
+
+     // Tratamento de validações de DTO
+     @ExceptionHandler(MethodArgumentNotValidException.class)
+     public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+
+          Map<String, String> errors = new HashMap<>();
+
+          // Captura todos os erros de validação e adiciona ao mapa
+          for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+               errors.put(error.getField(), error.getDefaultMessage());
+          }
+
+          return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+     }
+
+     // Tratamento de outras exceções genéricas
+     @ExceptionHandler(Exception.class)
+     public ResponseEntity<String> handleGenericException(Exception ex) {
+          return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro inesperado: " + ex.getMessage());
      }
 
 }
