51e85966f53932891d7749ee65c64f7109322e29
gabriellglrs <gabriellglrs@gmail.com>
2025-02-09 00:23:16 -0300

    feat: Habilita cache com Redis no Spring Boot
    - Adicionada anotação @EnableCaching para ativar o uso de cache no Spring Boot.
    - Configurado o Spring para usar Redis como provedor de cache, definindo as propriedades:
      - spring.cache.type=redis
      - spring.data.redis.host=localhost
      - spring.data.redis.port=6379

--- Back-End/taskify/src/main/java/com/dev/gabriellucas/taskify/TaskifyApplication.java
+++ Back-End/taskify/src/main/java/com/dev/gabriellucas/taskify/TaskifyApplication.java
@@ -2,8 +2,10 @@ package com.dev.gabriellucas.taskify;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching // Habilita o uso de cache
 public class TaskifyApplication {
 
 	public static void main(String[] args) {
