731440f3b5b03607d119ed604ddcb19fcc8ccf43
gabriellglrs <gabriellglrs@gmail.com>
2025-02-14 16:35:18 -0300

    feat: adicionar segurança ao projeto Spring Security, OAuth2
    - Adicionadas dependências do Spring Security, OAuth2 no `pom.xml`
    - Habilitado debug para o Spring Security
    - Adicionadas configurações de OAuth2 com Google
    - Movida a configuração do Swagger para o pacote correto

--- Back-End/taskify/src/main/java/com/dev/gabriellucas/taskify/configs/swagger/SwaggerConfig.java
+++ Back-End/taskify/src/main/java/com/dev/gabriellucas/taskify/configs/swagger/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.dev.gabriellucas.taskify.configs;
+package com.dev.gabriellucas.taskify.configs.swagger;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
