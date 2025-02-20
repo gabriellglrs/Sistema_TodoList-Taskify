a5e31c8edc6e2e8388945a40174bfb77cb6a0420
gabriellglrs <gabriellglrs@gmail.com>
2025-02-19 23:19:18 -0300

    feat: adiciona autenticação JWT no Swagger
    - Adiciona um esquema de segurança `BearerAuth` ao Swagger para autenticação via token JWT.
    - Inclui a configuração de `SecurityRequirement` para exigir autenticação em todos os endpoints.
    - Define `BearerAuth` como um esquema de segurança HTTP do tipo `bearer` com formato `JWT`.

--- Back-End/taskify/src/main/java/com/dev/gabriellucas/taskify/configs/swagger/SwaggerConfig.java
+++ Back-End/taskify/src/main/java/com/dev/gabriellucas/taskify/configs/swagger/SwaggerConfig.java
@@ -1,9 +1,12 @@
 package com.dev.gabriellucas.taskify.configs.swagger;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -24,6 +27,13 @@ public class SwaggerConfig {
                           .license(new License()
                                   .name("Licença Apache 2.0")
                                   .url("https://www.apache.org/licenses/LICENSE-2.0.html"))
-                  );
+                  ).addSecurityItem(new SecurityRequirement().addList("BearerAuth")) // Adiciona o esquema de segurança BearerAuth a todos os endpoints da API para autenticação via token JWT
+                  .components(new Components() // Adiciona os componentes de segurança da API (esquemas de segurança) para autenticação via token JWT (BearerAuth) e OAuth2
+                          .addSecuritySchemes("BearerAuth", new SecurityScheme() // Adiciona o esquema de segurança BearerAuth para autenticação via token JWT
+                                  .name("BearerAuth") // Nome do esquema de segurança
+                                  .type(SecurityScheme.Type.HTTP) // Tipo de esquema de segurança
+                                  .scheme("bearer") // Esquema de segurança
+                                  .bearerFormat("JWT") // Formato do token JWT
+                          ));
      }
 }
